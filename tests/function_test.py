b30c70b2a362412978b1888cc7df67f86844f758
terryqtt <terryqiutiantong@hotmail.com>
2024-03-29 01:34:25 -0400

    api key test w finder

--- tests/function_test.py
+++ tests/function_test.py
@@ -1,21 +1,13 @@
 import pytest
 import os
 import requests
+import sys
+sys.path.append('src\pyFilmFinder')
 from finder import Finder
+finder = Finder()
 API_KEY = os.getenv('API_KEY')
-def validate_api_key(API_KEY) -> bool:
-        """
-        Validate the API key by making a test request to the API endpoint.
-        """
-        api_url = f"https://api.themoviedb.org/3/authentication?api_key={API_KEY}"
-        try:
-            response = requests.get(api_url, timeout=10)
-            response.raise_for_status()
-            return response.status_code == 200
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to fetch films from API: {e}.")
-            return False
+finder.api_key = API_KEY
 def test_api_key():
     print(API_KEY)
     assert len(API_KEY) == 32, f"incorrect length for api key"
-    assert validate_api_key(API_KEY), f"api key not valid tests won't pass"    
+    assert finder.validate_api_key(), f"api key not valid tests won't pass"    
