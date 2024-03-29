6b366dff690d17df224ad62cbb51edc7198446f2
terryqtt <terryqiutiantong@hotmail.com>
2024-03-29 02:41:22 -0400

    try to fix finder import

--- tests/function_test.py
+++ tests/function_test.py
@@ -2,12 +2,13 @@ import pytest
 import os
 import requests
 import sys
-sys.path.append('src\\pyFilmFinder')
+sys.path.insert(0,'../src/pyFilmFinder')
 from finder import Finder
 finder = Finder()
+print("all ok")
 API_KEY = os.getenv('API_KEY')
 finder.api_key = API_KEY
 def test_api_key():
     print(API_KEY)
     assert len(API_KEY) == 32, f"incorrect length for api key"
-    assert finder.validate_api_key(), f"api key not valid tests won't pass"    
+    assert finder.validate_api_key(), f"api key not valid tests won't pass"  
