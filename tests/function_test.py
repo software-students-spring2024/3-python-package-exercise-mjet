83e349e27fd29319791f6b6c38dc2659ea0be4bb
terryqtt <terryqiutiantong@hotmail.com>
2024-03-30 01:09:25 -0400

    added tests for all functions(except parser)

--- tests/function_test.py
+++ tests/function_test.py
@@ -31,4 +31,22 @@ def test_find_films():
     #this result depends on the api call
     #TODO add result for incorrect parameters
 
+def test_search_films():
+    api_url = f"https://api.themoviedb.org/3/search/movie?language=en-US&page=1&query=iron&api_key={API_KEY}"
+    outputtemp = finder.get_films(api_url)
+    assert outputtemp == finder.search_films("iron"), f"incorrect output for search"
+    outputtemp2 ="Title:DetectiveConan:BlackIronSubmarineReleased:2023-04-14(ID:1047041)VoteAverage:6.8Description:ManyengineersfromaroundtheworldgatherattheInterpolmarinefacility"
+    assert outputtemp2.strip().replace(" ", "")[:50] == finder.search_films("iron").strip().replace(" ", "")[:50], f"incorrect output for search"
 
+def test_get_details():
+    fail_url = "nothing here"
+    failresponse = finder.get_details(fail_url)
+    assert failresponse[:31] == "Failed to fetch films from API:", f"error anticipated"
+    #TODO parser not implemented so not tested for this part
+
+def test_find_similar():
+    api_url = f"https://api.themoviedb.org/3/movie/27205/recommendations?api_key={API_KEY}"
+    outputtemp = finder.get_films(api_url)
+    assert outputtemp == finder.find_similar(27205)
+    outputtemp2 = "Title:TheDarkKnightReleased:2008-07-16(ID:155)VoteAverage:8.515Description:Batmanraisesthestakesinhiswaroncrime.WiththehelpofLt.JimGordonandDistrictAttorneyHarveyDent,Batmansetsouttodismantletheremainingcriminalorganizationsthatplaguethestreets.Thepartnershipprovestobeeffective,buttheysoonfindthemselvespreytoareignofchaosunleashedbyarisingcriminalmastermindknowntotheterrifiedcitizensofGothamastheJoker.Title:InterstellarReleased:2014-11-05(ID:157336)VoteAverage:8.431Description:Theadventuresofagroupofexplorerswhomakeuseofanewlydiscoveredwormholetosurpassthelimitationsonhumanspacetravelandconquerthevastdistancesinvolvedinaninterstellarvoyage.Title:ShutterIslandReleased:2010-02-14(ID:11324)VoteAverage:8.202Description:WorldWarIIsoldier-turned-U.S.MarshalTeddyDanielsinvestigatesthedisappearanceofapatientfromahospitalforthecriminallyinsane,buthiseffortsarecompromisedbytroublingvisionsandamysteriousdoctor.Title:AvatarReleased:2009-12-15(ID:19995)VoteAverage:7.58Description:Inthe22ndcentury,aparaplegicMarineisdispatchedtothemoonPandoraonauniquemission,butbecomestornbetweenfollowingordersandprotectinganaliencivilization.Title:TheHungerGamesReleased:2012-03-12(ID:70160)VoteAverage:7.205Description:EveryyearintheruinsofwhatwasonceNorthAmerica,thenationofPanemforceseachofitstwelvedistrictstosendateenageboyandgirltocompeteintheHungerGames.Parttwistedentertainment,partgovernmentintimidationtactic,theHungerGamesareanationallytelevisedeventinwhich“Tributes”mustfightwithoneanotheruntilonesurvivorremains.Pittedagainsthighly-trainedTributeswhohavepreparedfortheseGamestheirentirelives,KatnissisforcedtorelyuponhersharpinstinctsaswellasthementorshipofdrunkenformervictorHaymitchAbernathy.Ifshe’severtoreturnhometoDistrict12,Katnissmustmakeimpossiblechoicesinthearenathatweighsurvivalagainsthumanityandlifeagainstlove.Theworldwillbewatching."
+    assert outputtemp2.strip().replace(" ", "")[:50] == finder.find_similar(27205).strip().replace(" ", "")[:50], f"incorrect output for similar"
