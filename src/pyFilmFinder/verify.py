00173776fd8819cb1863d135133b3d6f7032a702
Morcupine <mje9832@nyu.edu>
2024-04-01 14:37:33 -0400

    updated movie search

--- src/pyFilmFinder/verify.py
+++ src/pyFilmFinder/verify.py
@@ -7,5 +7,5 @@ import find_similar
 
 finder = finder.Finder()
 
-target_film  = finder.get_film_dict(finder.get_film_id('cloverfield'))
+target_film  = finder.get_film_id('Die Hard')
 finder.rank_similar_by_overview(target_film)
